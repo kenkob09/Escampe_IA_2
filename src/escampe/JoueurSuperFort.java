7dc2a69152b1ae26adab97ff6fbf2938c9f08bf9
kenkob09 <kobayashitheone@hotmail.com>
2019-05-06 16:27:10 +0200

    test

--- src/escampe/JoueurSuperFort.java
+++ src/escampe/JoueurSuperFort.java
@@ -11,73 +11,7 @@ public class JoueurSuperFort implements IJoueur{
 	public String player;
 	private EscampeBoard board; //TODO : Peut-il etre static avec l'algo IA?
 	private Heuristique h = HeuristiquesEscampe.h;
-	private Heuristique h2 = HeuristiquesEscampe.h2;
-/*	private static Heuristique h1 = new Heuristique() {
-		
-		@Override
-		public float eval(Etat e) {
-			// TODO Auto-generated method stub
-			if (e instanceof EtatEscampe) {
-				EtatEscampe ee = (EtatEscampe) e;
-				int evalutation = 0; 
-				for(String p : ee.getWhite()) {
-					evalutation += EscampeBoard.get_i_from_string(p) + EscampeBoard.get_j_from_string(p);
-				}
-				for(String p : ee.getBlack()) {
-					evalutation -= EscampeBoard.get_i_from_string(p) + EscampeBoard.get_j_from_string(p);
-				}
-                return evalutation;
-            } else {
-                throw new Error("Cette heursitique ne peut s'appliquer que sur des EtatEscampe");
-            }
-		}
-	};
-	
-	private static Heuristique h2 = new Heuristique() {
-		
-		@Override
-		public float eval(Etat e) {
-			if (e instanceof EtatEscampe) {
-				EtatEscampe ee = (EtatEscampe) e;
-				int ret = 0;
-				int iOrigin,jOrigin;
-				String[] pions;
-				if(ee.getPlayer() == "blanc") {
-					pions = ee.getWhite();
-					
-					}
-				else {
-					pions = ee.getBlack();
-				}
-				iOrigin = EscampeBoard.get_i_from_string(pions[0]);
-				jOrigin = EscampeBoard.get_j_from_string(pions[0]);
-				
-				// On regarde la distance de chaques pions de la licorne
-				for(int i = 1; i < 6; i++) {
-					int iDistance = Math.abs(iOrigin - EscampeBoard.get_i_from_string(pions[i]));
-					int jDistance = Math.abs(jOrigin - EscampeBoard.get_j_from_string(pions[i]));
-					// Plus ils sont eloigne, plus la licorde est en danger
-					ret += (iDistance + jDistance);
-				}
-				return ret;
-            } else {
-                throw new Error("Cette heursitique ne peut s'appliquer que sur des EtatEscampe");
-            }
-		}
-	};
-	
-	private static Heuristique h3 = new Heuristique() {
-		
-		@Override
-		public float eval(Etat e) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-	};
-	*/
-	//AETOILE TROP LONG ! CEST UN ALGO QUI NE SARRETE PAS TANT QUON A PAS TROUVE LA SOLUTION ? 
-	//private static AEtoile algo = new AEtoile(h2);//met trop de temps*
-	
+	private Heuristique h2 = HeuristiquesEscampe.h2;	
 	private AlphaBeta algo;
 	
 	@Override
